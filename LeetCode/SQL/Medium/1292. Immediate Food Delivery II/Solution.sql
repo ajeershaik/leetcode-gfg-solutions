b37b611acde292debc03ed9efa9679ad8014805f
ajeer <ajeerahmad3016@gmail.com>
2026-09-02 12:15:01 +0530

    Add Medium SQL Solution for 1292. Immediate Food Delivery II from LeetCode

---
+++ LeetCode/SQL/Medium/1292. Immediate Food Delivery II/Solution.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+/*select sum(
+    if(order_date  = customer_pref_delivery_date,1,0)
+)as totaL_imm,count(order_date) as total_orders from delivery;*/
+
+select sum(
+    case 
+        when order_date = customer_pref_delivery_date then 1
+        else 0
+    end
+)*100/count(customer_id) as immediate_percentage from(
+    select customer_id,order_date,customer_pref_delivery_date from(
+    select customer_id,order_date,customer_pref_delivery_date,
+    row_number() over(partition by customer_id order by order_date) as rn
+    from delivery) as t
+    where rn = 1
+)as t1;
