247cc3824ed1767be64cac8eea10624c000ee877
ajeer <ajeerahmad3016@gmail.com>
2026-09-02 11:22:32 +0530

    Add Medium SQL Solution for 1934. Confirmation Rate from LeetCode

--- LeetCode/SQL/Medium/1934. Confirmation Rate/Solution.sql
+++ LeetCode/SQL/Medium/1934. Confirmation Rate/Solution.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 
-select s.user_id,rounsum(
+select s.user_id,round(sum(
     Case 
         when action = 'confirmed'  then 1
         else 0
-        end) / count(s.user_id) as confirmation_rate
+        end) / count(s.user_id),2) as confirmation_rate
 from Signups s left join 
       Confirmations c 
 on s.user_id = c.user_id 
