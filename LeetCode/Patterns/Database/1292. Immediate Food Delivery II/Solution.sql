a4401c0ecaf738f988fa78bf10dbd263d48b5d72
ajeer <ajeerahmad3016@gmail.com>
2026-09-02 12:17:01 +0530

    Add Medium SQL Solution for 1292. Immediate Food Delivery II from LeetCode

--- LeetCode/Patterns/Database/1292. Immediate Food Delivery II/Solution.sql
+++ LeetCode/Patterns/Database/1292. Immediate Food Delivery II/Solution.sql
@@ -4,12 +4,12 @@
     if(order_date  = customer_pref_delivery_date,1,0)
 )as totaL_imm,count(order_date) as total_orders from delivery;*/
 
-select sum(
+select round(sum(
     case 
         when order_date = customer_pref_delivery_date then 1
         else 0
     end
-)*100/count(customer_id) as immediate_percentage from(
+)*100/count(customer_id),2) as immediate_percentage from(
     select customer_id,order_date,customer_pref_delivery_date from(
     select customer_id,order_date,customer_pref_delivery_date,
     row_number() over(partition by customer_id order by order_date) as rn
