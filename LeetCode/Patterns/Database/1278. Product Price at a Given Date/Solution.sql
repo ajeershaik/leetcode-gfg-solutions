f5afd667b9ff46052efbbcd78cd399575a75ccfe
ajeer <ajeerahmad3016@gmail.com>
2026-09-04 14:56:45 +0530

    Add Medium SQL Solution for 1278. Product Price at a Given Date from LeetCode

--- LeetCode/Patterns/Database/1278. Product Price at a Given Date/Solution.sql
+++ LeetCode/Patterns/Database/1278. Product Price at a Given Date/Solution.sql
@@ -1,7 +1,16 @@
 # Write your MySQL query statement below
+
 select distinct p.product_id,
-if(t.np>=0,t.np,10)as price
- from products p left join 
-    (select product_id,new_price,change_date,max(new_price) as np from products
-    where datediff('2019-08-16',change_date)>=0 group by product_id) as t
-on p.product_id = t.product_id;
+    if(t.new_price>=0,t.new_price,10) as price 
+from products p
+left join (
+select  distinct product_id,
+    new_price,
+    change_date,
+    row_number() 
+        over(partition by product_id 
+        order by change_date desc) 
+    as rn from products
+where datediff('2019-08-16',change_date)>=0) as t 
+on p.product_id = t.product_id and rn=1;
+
