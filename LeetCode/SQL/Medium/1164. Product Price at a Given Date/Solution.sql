c74c5bce922c6e7a14b1bbbd349b171f80990676
ajeer <ajeerahmad3016@gmail.com>
2026-09-04 11:12:14 +0530

    Add Medium SQL Solution for 1164. Product Price at a Given Date from LeetCode

---
+++ LeetCode/SQL/Medium/1164. Product Price at a Given Date/Solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select distinct p.product_id,
+if(t.np>=0,t.np,10)as price
+ from products p left join 
+    (select product_id,new_price,change_date,max(new_price) as np from products
+    where datediff('2019-08-16',change_date)>=0 group by product_id) as t
+on p.product_id = t.product_id;
