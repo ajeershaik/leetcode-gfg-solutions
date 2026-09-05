a06b743ba08c5e317d15dc8f6ff72d0c25414a2d
ajeer <ajeerahmad3016@gmail.com>
2026-09-05 20:00:09 +0530

    Add Medium SQL Solution for 176. Second Highest Salary from LeetCode

--- LeetCode/SQL/Medium/176. Second Highest Salary/Solution.sql
+++ LeetCode/SQL/Medium/176. Second Highest Salary/Solution.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
 
-select max(if(rn=2,salary,null)) as SecondHighestSalary from(
-    select salary, rank() over(order by id) as rn from employee
-)as t where rn = 2;
+select (select distinct salary  
+from employee order by salary desc limit 1 offset 1)as SecondHighestSalary;
+
+/*select max(if(rn=2,salary,null)) as SecondHighestSalary from(
+    select salary, dense_rank() over(order by id) as rn from employee
+)as t where rn = 2;*/
