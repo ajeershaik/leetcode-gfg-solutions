f4e2a3cc0d02686b1568fb5ffeac5873b87a2316
ajeer <ajeerahmad3016@gmail.com>
2026-09-08 19:09:42 +0530

    Add Medium SQL Solution for 184. Department Highest Salary from LeetCode

---
+++ LeetCode/Patterns/Database/184. Department Highest Salary/Solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select Department,Employee,Salary from( 
+    select e.id,e.name as Employee,e.salary as Salary,e.departmentId,d.name as Department,dense_rank()
+    over(partition by d.name order by e.salary desc)as rn from 
+    employee e left join department d
+    on e.departmentId = d.id
+) as t where rn =1;
