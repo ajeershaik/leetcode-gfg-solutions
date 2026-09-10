62c99eb1628d963e7b4792367b87f48cdb0b3c7e
ajeer <ajeerahmad3016@gmail.com>
2026-09-10 20:09:29 +0530

    Add Medium Java Solution for 8. String to Integer (atoi) from LeetCode

---
+++ LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int myAtoi(String s) {
+        String s1 = s.stripLeading();
+        int n = s1.length();
+        StringBuilder result = new StringBuilder();
+        int dc = 0;
+        for(int i=0;i<n;i++){
+            char ch = s1.charAt(i);
+            if(Character.isLetter(ch) || (dc >0 && ch =='-' || ch =='+')){
+                if(result.length() == 0){
+                    return 0;
+                }
+                else{
+                    return Integer.parseInt(String.valueOf(result));
+                }
+            }
+            else{
+                
+                if(ch!='-' || ch!='+') 
+                {
+                    dc++;
+                }
+                if(ch != 0)
+                {
+                    result.append(ch);
+                }
+            }
+        }
+        return Integer.parseInt(String.valueOf(result));
+    }
+}
