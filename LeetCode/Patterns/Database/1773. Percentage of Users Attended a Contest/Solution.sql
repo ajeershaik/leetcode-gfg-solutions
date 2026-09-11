cb5d852bfd7780b143f4db48aa4ecc79b457b7ac
ajeer <ajeerahmad3016@gmail.com>
2026-09-11 21:01:43 +0530

    Add Easy SQL Solution for 1773. Percentage of Users Attended a Contest from LeetCode

---
+++ LeetCode/Patterns/Database/1773. Percentage of Users Attended a Contest/Solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+# Write your MySQL query s
+select contest_id,
+round(count(contest_id)*100/(select count(*) from users),2) as percentage from register group by contest_id
+order by percentage desc, contest_id asc;
