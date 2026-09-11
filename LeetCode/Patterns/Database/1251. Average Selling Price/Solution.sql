dd09739bba1cab9f5bdf7a196adbe22c2eb588ea
ajeer <ajeerahmad3016@gmail.com>
2026-09-11 20:08:54 +0530

    Add Easy SQL Solution for 1251. Average Selling Price from LeetCode

--- LeetCode/Patterns/Database/1251. Average Selling Price/Solution.sql
+++ LeetCode/Patterns/Database/1251. Average Selling Price/Solution.sql
@@ -4,5 +4,5 @@ select product_id,coalesce(round(sum(total_price)/sum(units),2),0)as
     average_price from(
     select p.product_id as product_id,p.start_date,p.end_date,p.price,u.purchase_date,u.units,
     p.price*u.units as total_price from prices p 
-    left join unitssold u on p.product_id = u.product_id and date_format(p.end_date,'%m') = date_format(u.purchase_date,'%m')
+    left join unitssold u on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
 )as t group by product_id;
