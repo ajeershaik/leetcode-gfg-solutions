b4b417652b81d8dd4006e57aec6403cbddd4ed3b
ajeer <ajeerahmad3016@gmail.com>
2026-09-12 11:06:21 +0530

    Add Easy SQL Solution for 1527. Patients With a Condition from LeetCode

--- LeetCode/Patterns/Database/1527. Patients With a Condition/Solution.sql
+++ LeetCode/Patterns/Database/1527. Patients With a Condition/Solution.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select patient_id,patient_name,conditions from patients where conditions like '%diab1%''''''''''''''';
+select patient_id,patient_name,conditions from patients where conditions like '%diab1%';
