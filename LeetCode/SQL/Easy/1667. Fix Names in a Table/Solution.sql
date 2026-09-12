925f737c7b0e144541f32a53dc2f647f6602e350
ajeer <ajeerahmad3016@gmail.com>
2026-09-12 10:36:50 +0530

    Add Easy SQL Solution for 1667. Fix Names in a Table from LeetCode

--- LeetCode/SQL/Easy/1667. Fix Names in a Table/Solution.sql
+++ LeetCode/SQL/Easy/1667. Fix Names in a Table/Solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 select USER_ID,concat(upper(left(name,1)),
-                LOWER(SUBSTR(NAME,2))) AS NAME FROM USERS;
+                LOWER(SUBSTR(NAME,2))) AS NAME FROM USERS ORDER BY USER_ID;
