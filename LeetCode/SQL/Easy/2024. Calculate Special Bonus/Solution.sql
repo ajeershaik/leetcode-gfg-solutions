b43020b0213fccb27db592159ee64ef85056ad14
ajeer <ajeerahmad3016@gmail.com>
2026-09-13 15:11:18 +0530

    Add Easy SQL Solution for 2024. Calculate Special Bonus from LeetCode

---
+++ LeetCode/SQL/Easy/2024. Calculate Special Bonus/Solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select employee_id,coalesce(sal,0) as bonus
+from(
+        select e.employee_id,e.salary,t.salary as sal from employees e left join(
+        select employee_id,name,salary from employees where employee_id%2 = 1 and name not like 'm%'
+    )as t on e.employee_id = t.employee_id
+)as t1;
