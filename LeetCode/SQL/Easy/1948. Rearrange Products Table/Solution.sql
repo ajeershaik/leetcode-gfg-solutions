70eefeada75a32c4ce8ecd1f5a1d00ba69782920
ajeer <ajeerahmad3016@gmail.com>
2026-09-13 20:02:46 +0530

    Add Easy SQL Solution for 1948. Rearrange Products Table from LeetCode

--- LeetCode/SQL/Easy/1948. Rearrange Products Table/Solution.sql
+++ LeetCode/SQL/Easy/1948. Rearrange Products Table/Solution.sql
@@ -4,6 +4,6 @@ select * from(
 union all
 select product_id,'store2' as store,case when store2 is not null then store2 end as price from products
 union all
-select product_id,'store3' as store,case when store1 is not null then store3 end as price from products
+select product_id,'store3' as store,case when store3 is not null then store3 end as price from products
 order by product_id
 )as t where price is not null order by product_id;
