1d04db90f359a8df24f8705c45b208c09c219a2f
ajeer <ajeerahmad3016@gmail.com>
2026-09-13 15:12:02 +0530

    Add Easy SQL Solution for 1873. Calculate Special Bonus from LeetCode

--- LeetCode/Patterns/Database/1873. Calculate Special Bonus/Solution.sql
+++ LeetCode/Patterns/Database/1873. Calculate Special Bonus/Solution.sql
@@ -2,7 +2,7 @@
 
 select employee_id,coalesce(sal,0) as bonus
 from(
-    select e.employee_id,e.salary,t.salary as sal from employees e left join(
-    select employee_id,name,salary from employees where employee_id%2 = 1 and name not like 'm%'
-)as t on e.employee_id = t.employee_id
-)as t1;
+        select e.employee_id,e.salary,t.salary as sal from employees e left join(
+        select employee_id,name,salary from employees where employee_id%2 = 1 and name not like 'm%'
+    )as t on e.employee_id = t.employee_id
+)as t1 order by employee_id;
