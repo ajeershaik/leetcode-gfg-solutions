e1ed1422900620601d23ed642b42638e37dd1072
ajeer <ajeerahmad3016@gmail.com>
2026-09-13 15:12:05 +0530

    Add Easy SQL Solution for 2024. Calculate Special Bonus from LeetCode

--- LeetCode/SQL/Easy/2024. Calculate Special Bonus/Solution.sql
+++ LeetCode/SQL/Easy/2024. Calculate Special Bonus/Solution.sql
@@ -5,4 +5,4 @@ from(
         select e.employee_id,e.salary,t.salary as sal from employees e left join(
         select employee_id,name,salary from employees where employee_id%2 = 1 and name not like 'm%'
     )as t on e.employee_id = t.employee_id
-)as t1;
+)as t1 order by employee_id;
