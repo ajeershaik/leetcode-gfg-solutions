cc78e0985ddb276bcd626e4dfa25a5fc5fc72450
ajeer <ajeerahmad3016@gmail.com>
2026-09-13 20:00:47 +0530

    Add Easy SQL Solution for 1795. Rearrange Products Table from LeetCode

---
+++ LeetCode/SQL/Easy/1795. Rearrange Products Table/Solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select * from(
+    select product_id,'store1' as store,case when store1 is not null then store1 end as price from products
+union all
+select product_id,'store2' as store,case when store2 is not null then store2 end as price from products
+union all
+select product_id,'store3' as store,case when store1 is not null then store3 end as price from products
+order by product_id
+)as t where price is not null;
