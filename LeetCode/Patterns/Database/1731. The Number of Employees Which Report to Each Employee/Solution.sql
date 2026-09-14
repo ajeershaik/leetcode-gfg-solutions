d3f55e0507d08ab29c6e6cfc4b463f6bef9e0907
ajeer <ajeerahmad3016@gmail.com>
2026-09-14 12:00:57 +0530

    Add Easy SQL Solution for 1731. The Number of Employees Which Report to Each Employee from LeetCode

--- LeetCode/Patterns/Database/1731. The Number of Employees Which Report to Each Employee/Solution.sql
+++ LeetCode/Patterns/Database/1731. The Number of Employees Which Report to Each Employee/Solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select emp_rep as employee_id,man_name as name,count(emp_rep) as reports_count,ceil(avg(emp_age)) as average_age
+select emp_rep as employee_id,man_name as name,count(emp_rep) as reports_count,round(avg(emp_age)) as average_age
 from(
     select e.employee_id as emp_id,e.name as emp_name,e.reports_to as emp_rep,
 e.age as emp_age,m.name as man_name,m.reports_to as man_rep,m.age as man_age
